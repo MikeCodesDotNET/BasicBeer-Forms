efd091be624e1cc3ac3d275994a30ee4259e1f1f
Colby Williams <colbylwilliams@gmail.com>
2016-01-20 14:11:26 -0500

    bitrise test 2

--- Helpers/Keys.cs
+++ Helpers/Keys.cs
@@ -1,13 +1,7 @@
-﻿using System;
-
-using Xamarin.Forms;
-
-namespace BasicBeerForms.Helpers
+﻿namespace BasicBeerForms.Helpers
 {
-    public class Keys 
-    {
-        public static string XamarinInsightsKey = "304fcc396a1a460329a16dc9adb4abf9a85362ef";
-    }
+	public static class Keys
+	{
+		public static string XamarinInsightsKey = "304fcc396a1a460329a16dc9adb4abf9a85362ef";
+	}
 }
-
-
