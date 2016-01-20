99034d3d4194836b2cb49f9d7b922493d8ff92b6
Colby Williams <colbylwilliams@gmail.com>
2016-01-20 15:30:33 -0500

    Fixed UITest paths for bitrise build

--- BasicBeerForms.cs
+++ BasicBeerForms.cs
@@ -4,28 +4,31 @@ using Xamarin.Forms;
 
 namespace BasicBeerForms
 {
-    public class App : Application
-    {
-        public App()
-        {
-            // The root page of your application
-            MainPage = new NavigationPage(new Views.SearchPage()){ Title = "Search", BarBackgroundColor = Color.FromHex("15A9FF"), BarTextColor = Color.White };
-        }
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new NavigationPage (new Views.SearchPage ()) {
+				Title = "Search",
+				BarBackgroundColor = Color.FromHex("15A9FF"),
+				BarTextColor = Color.White
+			};
+		}
 
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
 
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
 
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
-    }
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
 }
-
